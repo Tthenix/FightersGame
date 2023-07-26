9cdcbd2936f37df386624cb498eb9828f10f014d
TtheNix <nahuelfacundox@gmail.com>
2023-07-26 12:05:43 -0300

    fix:UI

--- Assets/scripts/Opciones.cs
+++ Assets/scripts/Opciones.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class Opciones : ScriptableObject
+public class Opciones : MonoBehaviour
 {
     [SerializeField] private AudioMixer audioMixer;
 
